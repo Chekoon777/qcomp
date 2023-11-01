3031c280454b71e222d3afdbbbcec9f5ff471ab3
Chekoon777 <chekong@dgist.ac.kr>
2023-11-01 20:21:00 +0900

    Initial commit

---
+++ src/main/java/qd/qcomp/qcompplugin/Qcomp.java
@@ -0,0 +1,17 @@
+package qd.qcomp.qcompplugin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Qcomp extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+        System.out.println("My first plugin has started!!");
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
